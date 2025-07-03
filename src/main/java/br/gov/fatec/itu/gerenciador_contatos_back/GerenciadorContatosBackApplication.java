f074f51ce904727d782756d4e0698ce10cdd694d
VitorSolerAguilar <vitorsoleraguilar49@gmail.com>
2025-07-02 23:06:43 -0300

    Initial commit

---
+++ src/main/java/br/gov/fatec/itu/gerenciador_contatos_back/GerenciadorContatosBackApplication.java
@@ -0,0 +1,13 @@
+package br.gov.fatec.itu.gerenciador_contatos_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorContatosBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorContatosBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/gov/fatec/itu/gerenciador_contatos_back/GerenciadorContatosBackApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gov.fatec.itu.gerenciador_contatos_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorContatosBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
